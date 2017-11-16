84a455375e0367daa05dc5b1512e11c708493757
Dremalka <dremalka@ua.fm>
2017-11-16 09:16:25 +0700

    изменил структуру проекта

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import sys
+import configparser
 
 
 class Engine(object):
@@ -68,14 +69,62 @@ class Runner(object):
         print('End game')
 
 
+class ConfigApp(object):
+    def make_sample(self):
+        '''make sample config'''
+        conf = configparser.RawConfigParser()
+
+        conf.set("", "name", "Name sample")
+
+        conf.add_section("scene")
+        conf.set("scene", "scene_cmd", "scenes\scene_sample.py")
+
+        conf.add_section("mybot")
+        conf.set("mybot", "mybot_cmd", "scenes\mybot\mybot_sample.py")
+
+        conf.add_section("enemybot")
+        conf.set("enemybot", "enemybot_cmd", "scenes\enemybot\enemybot_sample.py")
+
+        conf.write(open("sample_config.conf", "w"))
+        print("Make sample config. Name: sample_config.conf")
+
+    def get_config(self, name_file):
+        '''get config from file'''
+        conf = configparser.RawConfigParser()
+        conf.read(name_file)
+        if not conf.has_option("", "name") or not conf.has_section("scene")\
+                or not conf.has_section("mybot") or not conf.has_section("enemybot"):
+            print("Wrong config file!")
+            return False
+        else:
+            return True
+
+    def get_usage(self):
+        '''print usage command'''
+        print('''usage:\n--make-sample-config  -  make sample config\n''')
+
+
 def main():
-    try:
-        args = sys.argv
-        assert len(args) > 2
-        runner = Runner(*args[1:])
-        runner.run()
-    except KeyboardInterrupt:
+    args = sys.argv
+    cfg = ConfigApp()
+    if len(args) == 1:  # запуск без аргументов. Вывести описание возможных аргументов.
+        cfg.get_usage()
         return
+
+    if args[1] == "--make-sample-config":   # пользователь попросил создать пример конфигурационного файла
+        cfg.make_sample()
+        return
+    else:
+        if cfg.get_config(args[1]):
+            print("Config OK!")
+
+#    try:
+#        assert len(args) > 2
+#        runner = Runner(*args[1:])
+#        runner.run()
+#    except KeyboardInterrupt:
+#        return
+
 
 if __name__ == "__main__":
     main()
